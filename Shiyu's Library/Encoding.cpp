d6d6102ccb9e7e625d246e9a5e452533e3bc7e90
时雨 <270598250@qq.com>
2023-01-13 13:10:34 +0800

    对部分函数的代码进行了规范化操作

--- Shiyu's Library/Encoding.cpp
+++ Shiyu's Library/Encoding.cpp
@@ -213,6 +213,33 @@ string Encoding::UnicodeToUTF8(wstring& wstr)
     }
     return ret;
 }
+string Encoding::UnicodeToANSI(const wstring& wstr)
+{
+    std::string ret;
+    std::mbstate_t state = {};
+    const wchar_t* src = wstr.data();
+    size_t len = std::wcsrtombs(nullptr, &src, 0, &state);
+    if (static_cast<size_t>(-1) != len) {
+        std::unique_ptr< char[] > buff(new char[len + 1]);
+        len = std::wcsrtombs(buff.get(), &src, len, &state);
+        if (static_cast<size_t>(-1) != len) {
+            ret.assign(buff.get(), len);
+        }
+    }
+    return ret;
+}
+string Encoding::UTF8ToANSI(string& str)
+{
+    wstring a;
+    a = Encoding::UTF8ToUnicode(str);
+    return Encoding::UnicodeToANSI(a);
+}
+string Encoding::ANSIToUTF8(string& str)
+{
+    wstring a;
+    a = Encoding::ANSIToUnicode(str);
+    return Encoding::UnicodeToUTF8(a);
+}
 wstring Encoding::UTF8ToUnicode(string& str)
 {
     std::wstring ret;
@@ -222,6 +249,21 @@ wstring Encoding::UTF8ToUnicode(string& str)
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
+    }
+    return ret;
+}
+wstring Encoding::ANSIToUnicode(const string& str)
+{
+    std::wstring ret;
+    std::mbstate_t state = {};
+    const char* src = str.data();
+    size_t len = std::mbsrtowcs(nullptr, &src, 0, &state);
+    if (static_cast<size_t>(-1) != len) {
+        std::unique_ptr< wchar_t[] > buff(new wchar_t[len + 1]);
+        len = std::mbsrtowcs(buff.get(), &src, len, &state);
+        if (static_cast<size_t>(-1) != len) {
+            ret.assign(buff.get(), len);
+        }
     }
     return ret;
 }
